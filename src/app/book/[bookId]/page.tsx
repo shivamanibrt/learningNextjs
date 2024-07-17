dec31d8aa4121595e20f1cb16c70148ed6cda143
shivamanibrt <shivamanibrt03350@gmail.com>
2024-07-18 07:09:24 +1000

    Download button added to download files

--- src/app/book/[bookId]/page.tsx
+++ src/app/book/[bookId]/page.tsx
@@ -1,6 +1,7 @@
 import { Book } from "@/types";
 import Image from "next/image";
 import React from "react";
+import DownloadButton from "./components/DownloadButton";
 
 const SingleBookPage = async ({ params }: { params: { bookId: string } }) => {
     let book: Book | null = null;
@@ -19,23 +20,26 @@ const SingleBookPage = async ({ params }: { params: { bookId: string } }) => {
     }
 
     return (
-        <div className='mx-auto grid max-w-6xl grid-cols-3 gap-10 px-5 py-10'>
-            <div className='col-span-2 pr-16 text-primary-950'>
-                <h2 className='mb-5 text-5xl font-bold leading-[1.1]'>
-                    {book.name}
-                </h2>
-                <span className='font-semibold'>by {book.username}</span>
-                <p className='mt-5 text-lg leading-8'>{book.email}</p>
-            </div>
-            <div className='flex justify-end'>
-                <Image
-                    src={book.image}
-                    alt={book.title ? book.title : "Book Image"}
-                    width={0}
-                    height={0}
-                    sizes={"100vw"}
-                    style={{ width: "auto", height: "12rem" }}
-                />
+        <div className='p-4'>
+            <div className='mx-auto grid h-96 max-w-5xl grid-cols-3 gap-10 px-5 py-10 border shadow p-2'>
+                <div className='col-span-2 pr-16 text-primary-950'>
+                    <h2 className='mb-5 text-5xl font-bold leading-[1.1]'>
+                        {book.name}
+                    </h2>
+                    <span className='font-semibold'>by {book.username}</span>
+                    <p className='mt-5 text-lg leading-8'>{book.email}</p>
+                    <DownloadButton fileLink={book.website} />
+                </div>
+                <div className='flex justify-end'>
+                    <Image
+                        src={book.image}
+                        alt={book.title ? book.title : "Book Image"}
+                        width={0}
+                        height={0}
+                        sizes={"100vw"}
+                        style={{ width: "auto", height: "12rem" }}
+                    />
+                </div>
             </div>
         </div>
     );
